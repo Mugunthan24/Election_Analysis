a7da5d1d9c628cd726654085ae0538abbedb73c4
Mugunthan Rengarajah <mrengarajah@gmail.com>
2022-01-17 17:35:00 -0500

    Adding the election audit code.

--- PyPoll2.py
+++ PyPoll2.py
@@ -1,13 +1,51 @@
 import csv
 import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-file_to_load = os.path.join( "resources", "election_results.csv")
+# Initialize a total vote counter.
+total_votes = 0
 
+# Candidate Options
+candidate_options = []
+candidate_votes = []
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-    reader_file = csv.reader(election_data)
-    for rows in election_data:
-        print(rows)
+    # Read the header row.
+    headers = next(file_reader)
+
+# Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes = total_votes + 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+    
+    
+    for vote in range(len(candidate_options)):
+        for row in file_reader:
+            candidate_name = row[2]
+            if candidate_name == candidate_options[vote]:
+                candidate_votes[vote] = candidate_votes[vote] + 1
+                print(candidate_votes[vote])
+                
+                
+        
+ 
+
+# Print the candidate list.
+print(candidate_votes)
 
 
 
